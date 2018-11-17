c7fe0a474a86e17c24ee7e9d58097c5ad87e55e8
Awsome-Areej <41355424+Awsome-Areej@users.noreply.github.com>
2018-11-17 13:10:01 -0800

    Create I-am-awesome.py

---
+++ I-am-awesome.py
@@ -0,0 +1,6 @@
+def iamawesome():
+    name = input ("What's your name?")
+    for awesome in range(101):
+        print "===================="
+        print name + " is awesome!"
+        print "===================="
