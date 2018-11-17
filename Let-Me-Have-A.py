c704d78b48d45f05deab78c03c52af362f63439a
Awsome-Areej <41355424+Awsome-Areej@users.noreply.github.com>
2018-11-17 13:23:50 -0800

    Create Let-Me-Have-A.py
    This would be mostly intended for kids, but..... you can still use it (if it still interests you).You can call the function as "let_me_have_a()".

---
+++ Let-Me-Have-A.py
@@ -0,0 +1,23 @@
+def let_me_have_a():
+    what3 = input ("What do you want?")
+    print ("You want a " + what3)
+    right1 = input ("Is that right?")
+    if right1 == "yes":
+        print " Here is what will happen;..."
+        print "I will keep repeating something for a long time, and you can show this code to whoever you are trying to ask! "
+        number = input ("How many times do you want me to repeat it?")
+        for happen in range(number):
+            print "=================="
+            print ("Please can you get me a " + what3)
+            print "Please,Please,Please,Please,Please,Please,Please,Please,Please,Please,Please,Please,Please,Please,Please,Please!"
+            print "=================="
+    else:
+        print "Let's try again,..."
+        what3 = input ("What do you want?")
+        print ("You want a " + what3)
+        right1 = input ("Is that right?")
+         
+        
+        
+        
+        
